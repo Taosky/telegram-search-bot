f0d21a3264d9d03120993adba49f2ab8c482708d
Taosky <t@firefoxcn.net>
2018-09-23 21:03:12 +0800

    error handler

--- robot.py
+++ robot.py
@@ -13,7 +13,7 @@ dispatcher = updater.dispatcher
 job = updater.job_queue
 
 # handle error
-# dispatcher.add_error_handler(error_callback)
+dispatcher.add_error_handler(error_callback)
 # handle mods
 for handler in mod_handlers:
     dispatcher.add_handler(handler)
